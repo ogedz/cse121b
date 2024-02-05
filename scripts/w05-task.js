74553dea7e3a320f0c66214f1e975b8ce6b4e51d
ogedz <ogedengbe_m@yahoo.com>
2024-02-05 12:28:26 +0100

    Week 5 task

---
+++ scripts/w05-task.js
@@ -0,0 +1,69 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.getElementById('temples');
+let templeList = [];
+
+/* async displayTemples Function */
+const displayTemples = temples => {
+    temples.forEach(temple => {
+      const article = document.createElement('article');
+      const h3 = document.createElement('h3');
+      const img = document.createElement('img');
+  
+      h3.textContent = temple.templeName;
+      img.src = temple.imageUrl;
+      img.alt = temple.location;
+  
+      article.appendChild(h3);
+      article.appendChild(img);
+  
+      templesElement.appendChild(article);
+    });
+};
+
+/* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    try {
+        const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+        templeList = await response.json();
+        displayTemples(templeList);
+    } catch (error) {
+        console.error('Error fetching temple data:', error);
+    }
+};
+  
+/* reset Function */
+const reset = () => {
+    templesElement.innerHTML = ''; 
+};
+
+/* filterTemples Function */
+const filterTemples = temples => {
+    reset(); 
+    const filter = document.getElementById('filtered').value;
+  
+    switch (filter) {
+      case 'utah':
+        displayTemples(temples.filter(temple => temple.location.includes('Utah')));
+        break;
+      case 'notutah':
+        displayTemples(temples.filter(temple => !temple.location.includes('Utah')));
+        break;
+      case 'older':
+        const olderTemples = temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1));
+        displayTemples(olderTemples);
+        break;
+      case 'all':
+      default:
+        displayTemples(temples);
+        break;
+    }
+};
+
+getTemples();
+
+/* Event Listener */
+document.getElementById('filtered').addEventListener('change', () => {
+    filterTemples(templeList);
+});
